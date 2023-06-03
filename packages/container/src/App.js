46f941b00d0c444514978cd77cf300bf93534930
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-06-03 12:59:24 +0800

    test container change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return(
+  return (
     <div>
-      <h1>Hi there!</h1>
+      <h1>Hi there yat!</h1>
       <hr />
       <MarketingApp />
     </div>
